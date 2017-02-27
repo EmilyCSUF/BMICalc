7f0310f93749e2911d46c44bcdaf06d6cc2718ed
Emily Michelle Lin <LinE@emilys-macbook-pro.local>
2017-02-26 17:11:04 -0800

    Initial Commit

---
+++ BMICalcTests/BMICalcTests.swift
@@ -0,0 +1,36 @@
+//
+//  BMICalcTests.swift
+//  BMICalcTests
+//
+//  Created by Emily Michelle Lin on 2/26/17.
+//  Copyright © 2017 Emily Michelle Lin. All rights reserved.
+//
+
+import XCTest
+@testable import BMICalc
+
+class BMICalcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
