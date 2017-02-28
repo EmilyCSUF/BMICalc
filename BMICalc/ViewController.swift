04030f902ab0d98b9e32fe08c27985ce02cf040e
Emily Michelle Lin <LinE@emilys-macbook-pro.local>
2017-02-27 19:31:48 -0800

    Test

--- BMICalc/ViewController.swift
+++ BMICalc/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Heighttext: UILabel!
+    @IBOutlet weak var Weighttext: UILabel!
+    @IBOutlet weak var MetricText: UILabel!
+    @IBOutlet weak var Heightinput: UITextField!
+    @IBOutlet weak var Weightinput: UITextField!
+    @IBOutlet weak var Results: UILabel!
+    
+    @IBAction func MetricSwitch(_ sender: UISwitch) {
+        //metric: height- Centimeters weight: kilograms
+        //standard: height: inches weight: Pounds
+        var switchstatus = 0
+        if sender.isOn {
+            Weighttext.text = "Weight(kg)"
+            Heighttext.text = "Height(cm)"
+            switchstatus = 0 //metric is on
+        }
+        else{
+            Weighttext.text = "Weight(lb)"
+            Heighttext.text = "Height(in)"
+            switchstatus = 1 //metric is not on
+        }
+        
+        Heighttext.text = "Height."
+ 
+    }
+
+    @IBOutlet weak var ImageDisplay: UIImageView!
+
+    @IBAction func Calculate(_ sender: AnyObject) {
+        let x = Double(Heighttext.text!)!
+        let y = Double(Weighttext.text!)!
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
